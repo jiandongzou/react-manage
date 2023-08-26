55f61d64c1a411c2626bbdba575a950be82ac796
zoujiandong <10130823232@qq.com>
2023-08-26 22:54:10 +0800

    提交

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import { StyleProvider } from '@ant-design/cssinjs';
 import {store,persistor} from "@/redux/index"
 import { Provider } from "react-redux"
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter,HashRouter } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/lib/integration/react'
 import { setupProdMockServer } from '@/mock/_index';
 
@@ -16,14 +16,14 @@ if (import.meta.env.MODE === 'production') {
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
   <StyleProvider hashPriority="high">
-    <BrowserRouter>
+    <HashRouter>
       <Provider store={store}>
       <PersistGate persistor={persistor}>
           <App />
       </PersistGate>
        
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
     </StyleProvider>
   </React.StrictMode>
 )
